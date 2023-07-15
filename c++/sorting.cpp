c85189c6dabcaedb8adc58be0c0c341f69e1925d
vietdung07 <nguyendung171207@gmail.com>
2023-07-16 02:13:36 +0700

    reset old ver

--- c++/sorting.cpp
+++ c++/sorting.cpp
@@ -1,30 +1,14 @@
 #include <iostream>
-#include <cmath>
 #include <typeinfo>
-#include <sstream>
-#include <string>
 using namespace std;
-int add_digit(int a, int b)
-{
-    int count = 0;
-    int tmpb = b;
-    while (tmpb > 0)
-    {
-        count++;
-        tmpb /= 10;
-    }
-    return (a * static_cast<int>(pow(10.0f, static_cast<float>(count))) + b) * 10 + count;
-}
 namespace temp
 {
     int Qpart(int arr[], const int begin, const int end)
     {
-        int count = 0;
         int i = begin, j = end;
         const int mid = (begin + end) / 2;
         int pivot = mid;
         {
-            count++;
             int max = begin, min = begin;
             if (arr[max] < arr[mid])
                 max = mid;
@@ -41,7 +25,6 @@ namespace temp
         }
         while (i != pivot || j != pivot)
         {
-            count++;
             if (arr[i] <= arr[pivot] && i < pivot)
                 i++;
             if (arr[j] >= arr[pivot] && j > pivot)
@@ -55,30 +38,22 @@ namespace temp
                     pivot = i;
             }
         }
-        return add_digit(count, pivot);
+        return pivot;
     }
-    int merge(int arr[], int begin, int mid, int end)
+    void merge(int arr[], int begin, int mid, int end)
     {
-        int count = 0;
         const int size1 = mid - begin + 1;
         const int size2 = end - mid;
         int *arr1 = new int[size1];
         int *arr2 = new int[size2];
         for (int i = 0; i < size1; i++)
-        {
-            count++;
             arr1[i] = arr[begin + i];
-        }
         for (int i = 0; i < size2; i++)
-        {
-            count++;
             arr2[i] = arr[mid + 1 + i];
-        }
         int index_1 = 0, index_2 = 0;
         int index = begin;
         while (index_1 < size1 && index_2 < size2)
         {
-            count++;
             if (arr1[index_1] < arr2[index_2])
             {
                 arr[index] = arr1[index_1];
@@ -93,105 +68,73 @@ namespace temp
         }
         while (index_1 < size1)
         {
-            count++;
             arr[index] = arr1[index_1];
             index++;
             index_1++;
         }
         while (index_2 < size2)
         {
-            count++;
             arr[index] = arr2[index_2];
             index++;
             index_2++;
         }
         delete[] arr1;
         delete[] arr2;
-        return count;
     }
 } // namespace temp
 namespace sorting
 {
     void Quick_sort(int arr[], int begin, int end)
     {
-        static int count = 0;
         if (begin >= end)
             return;
-        int tmp = temp::Qpart(arr, begin, end);
-        int mod = static_cast<int>(pow(10.0f, static_cast<float>(tmp % 10 + 1)));
-        count += tmp / mod;
-        int pivot = (tmp % mod) / 10;
+        int pivot = temp::Qpart(arr, begin, end);
         Quick_sort(arr, begin, pivot - 1);
         Quick_sort(arr, pivot + 1, end);
-        cout << count << endl;
     }
-    int Merge_sort(int arr[], int begin, int end)
+    void Merge_sort(int arr[], int begin, int end)
     {
-        int count = 0;
         if (begin >= end)
-            return count;
+            return;
         int mid = (begin + end) / 2;
         Merge_sort(arr, begin, mid);
         Merge_sort(arr, mid + 1, end);
-        count += temp::merge(arr, begin, mid, end);
-        return count;
+        temp::merge(arr, begin, mid, end);
     }
     void Bubble_sort(int arr[], int size)
     {
-        int count = 0;
         for (int i = 0; i < size; i++)
-        {
-            count++;
             for (int j = 0; j < size - 1; j++)
-            {
-                count++;
                 if (arr[j] > arr[j + 1])
                     swap(arr[j], arr[j + 1]);
-            }
-        }
     }
     void Selection_sort(int arr[], int size)
     {
-        int count = 0;
         for (int i = 0; i < size; i++)
         {
-            count++;
-            int min = i;
+            int max = i;
             for (int j = i + 1; j < size; j++)
-            {
-                count++;
-                if (arr[j] < arr[min])
-                    min = j;
-            }
-            swap(arr[i], arr[min]);
+                if (arr[i] > arr[j])
+                    max = j;
+            arr[i] = arr[max];
         }
-        cout << count << endl;
     }
     void Insertion_sort(int arr[], int size)
     {
-        int count = 0;
         for (int i = 0; i < size; i++)
         {
-            count++;
             int t = 0;
             for (; t < i; t++)
-            {
-                count++;
                 if (arr[t] > arr[i])
                     break;
-            }
             if (t < i)
             {
                 int temp = arr[i];
                 for (int j = i - 1; j >= t; j--)
-                {
-                    count++;
                     arr[j + 1] = arr[j];
-                }
                 arr[t] = temp;
             }
         }
-        cout << count << endl;
     }
 }
 void printArr(int arr[], int size)
@@ -207,8 +150,8 @@ void printArr(int arr[], int size)
 
 int main()
 {
-    int a[26] = {88, 18, 100, 92, 98, 49, 30, 72, 93, 94, 97, 12, 49, 66, 61, 62, 100, 64, 40, 97, 37, 1, 22, 65, 18, 12};
-    sorting::Quick_sort(a, 0, 25);
-    printArr(a, 26);
+    int a[13] = {88, 18, 100, 92, 98, 49, 30, 72, 93, 94, 97, 12, 49};
+    sorting::Selection_sort(a, 13);
+    printArr(a, 13);
     return 0;
 }
